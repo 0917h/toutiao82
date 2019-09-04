2576398b719cb0925dae12100e831517d18b8b97
huaijia <huaijia@qq.com>
2019-09-04 19:48:48 +0800

    安装

---
+++ src/views/Login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+      Login
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang= 'less' scoped>
+.login {
+  background: url(../../assets/img/login_bg.jpg);
+  height: 100vh;
+}
+</style>
