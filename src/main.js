a0ed27cd2a1a24ab4c893e23e3f32f036d6b4c6a
huaijia <huaijia@qq.com>
2019-09-04 22:10:29 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
+import axios from 'axios'
+Vue.prototype.$axios = axios // 给Vue对象的原型属性赋值 那么所有Vue实例自动拥有axios
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
