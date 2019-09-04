b6f7cbb3c458ceb8d41b82b28173edacf1c8556d
huaijia <huaijia@qq.com>
2019-09-04 20:20:31 +0800

    登录

--- src/router.js
+++ src/router.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home'
+// import Home from './views/home'
 import Login from './views/Login/login'
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'home',
-    component: Home
-  }, {
-    path: '/',
-    name: 'login',
-    component: Login
-  }
+  routes: [
+    // {
+    // path: '/',
+    // name: 'home',
+    // component: Home
+    // },
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    }
     // {
     //   path: '/about',
     //   name: 'about',
--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="login">
-      Login
+      <el-card class="login-card">
+          <div class="logo">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+        <!-- 表单组件 el-from 表单容器 -->
+        <el-form style="margin-top:15px">
+            <el-form-item>
+                <el-input></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input style="width:250px"></el-input>
+                <el-button style="float:right">发送验证码</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+            </el-form-item>
+            <el-form-item>
+                <el-button style="width:100%" type="primary">登录</el-button>
+            </el-form-item>
+        </el-form>
+
+      </el-card>
   </div>
 </template>
 
@@ -12,5 +33,19 @@ export default {}
 .login {
   background: url(../../assets/img/login_bg.jpg);
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-size: cover;
+  align-items: center;
+  .login-card {
+    width: 420px;
+    height: 330px;
+    .logo {
+      text-align: center;
+      img {
+        height: 40px;
+      }
+    }
+  }
 }
 </style>
