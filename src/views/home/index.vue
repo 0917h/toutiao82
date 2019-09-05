73b7f70d0f9357d14c0e6cc6b3327c7581e0c177
huaijia <huaijia@qq.com>
2019-09-05 15:11:48 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,17 @@
 <template>
-    <dir>
-        home
-    </dir>
+<!-- 最外层容器 -->
+    <el-container>
+        <!-- 左侧导航 -->
+        <el-aside>左侧</el-aside>
+
+        <!-- 右侧 -->
+        <el-container>
+            <!-- 头部 -->
+            <el-header> 头部</el-header>
+            <!-- 主要区域 -->
+            <el-main>主体</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
