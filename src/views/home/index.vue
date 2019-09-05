6b7309b385cb65f043190fc39ead4e2634003f4c
huaijia <huaijia@qq.com>
2019-09-05 17:10:22 +0800

    头部结构及下拉菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,12 +2,18 @@
 <!-- 最外层容器 -->
     <el-container>
         <!-- 左侧导航 -->
-        <el-aside>左侧</el-aside>
+        <el-aside style='width: 200px'>
+            <layout-aside></layout-aside>
+        </el-aside>
 
         <!-- 右侧 -->
         <el-container>
             <!-- 头部 -->
-            <el-header> 头部</el-header>
+            <el-header>
+                <layout-header>
+
+                </layout-header>
+            </el-header>
             <!-- 主要区域 -->
             <el-main>主体</el-main>
         </el-container>
@@ -15,7 +21,14 @@
 </template>
 
 <script>
-export default {}
+import layoutAside from '../../components/home/layout-aside.vue'
+import layoutHeader from '../../components/home/layout-header.vue'
+export default {
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
+}
 </script>
 
 <style>
