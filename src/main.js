c12574fc60fbdfddb555fe657f71d04af6e49ffc
huaijia <huaijia@qq.com>
2019-09-05 18:56:24 +0800

    统一注册全局插件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import './style/index.less'
+import ElementUI from 'element-ui' // 框架组件
+import Component from './components' // 自定义组件
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 import axios from 'axios'
-Vue.prototype.$axios = axios // 给Vue对象的原型属性赋值 那么所有Vue实例自动拥有axios
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值基础地址
+Vue.prototype.$axios = axios // 给Vue对象的原型属性赋值 那么所有Vue实例自动拥有axios
 Vue.config.productionTip = false
-Vue.use(ElementUI)
-
+Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
+Vue.use(Component) // 全局注册自定义组件
 new Vue({
   router,
   render: h => h(App)
